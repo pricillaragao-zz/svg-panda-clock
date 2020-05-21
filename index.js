a851420befd2d09793ea5630701bc73c52b40f01
Pricilla Aragao <pricillapatricia@gmail.com>
2020-05-21 00:14:16 -0300

    add the seconds hand

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
 
-const clockSecond = document.querySelector('.clock-second');
-const clockMinute = document.querySelector('.clock-minute');
-const clockHour = document.querySelector('.clock-hour');
+// const clockSecond = document.querySelector('.clock-second');
+// const clockMinute = document.querySelector('.clock-minute');
+// const clockHour = document.querySelector('.clock-hour');
 
 
-function setDate() {
+// function setDate() {
     const now = new Date();
     // 1 - seconds
     const seconds = now.getSeconds();
     const secondsDegrees = ((seconds / 60) * 360) + 90;  // 0s = 0degress and 59s = 360degrees    
-    clockSecond.style.transform = `rotate(${secondsDegrees}deg)`;
-    console.log(seconds);
+    const sec = document.querySelector("#sec-hand > animateTransform");
+    sec.setAttribute("from", `${secondsDegrees} 579.15 400`);
+    sec.setAttribute("to", `${secondsDegrees + 360} 579.15 400`);
+    console.log(sec)
 
-    // 2 - minutes 
-    const minutes = now.getMinutes();
-    const minutesDegrees = ((minutes / 60) * 360) + 90;
-    clockMinute.style.transform = `rotate(${minutesDegrees}deg)`;
+//     // 2 - minutes 
+//     const minutes = now.getMinutes();
+//     const minutesDegrees = ((minutes / 60) * 360) + 90;
+//     clockMinute.style.transform = `rotate(${minutesDegrees}deg)`;
    
-    // 3 - hours
-    const hours = now.getHours();
-    const hoursDegrees = ((hours / 12) * 360) + 90;
-    clockHour.style.transform = `rotate(${hoursDegrees}deg)`; 
+//     // 3 - hours
+//     const hours = now.getHours();
+//     const hoursDegrees = ((hours / 12) * 360) + 90;
+//     clockHour.style.transform = `rotate(${hoursDegrees}deg)`; 
 
 
-}
+// }
 
-setInterval(setDate, 1000);
+// setInterval(setDate, 1000);
 
 
+from="${shifter(minuteAngle)}"
+to="${shifter(minuteAngle + 360)}"
