6094afdd5dc3f8a263f4209411d67bdc44c0f8b2
Pricilla Aragao <pricillapatricia@gmail.com>
2020-05-22 16:01:45 -0300

    add the clock hands

--- index.js
+++ index.js
@@ -10,19 +10,25 @@
     const seconds = now.getSeconds();
     const secondsDegrees = ((seconds / 60) * 360) + 90;  // 0s = 0degress and 59s = 360degrees    
     const sec = document.querySelector("#sec-hand > animateTransform");
-    sec.setAttribute("from", `${secondsDegrees} 579.15 400`);
-    sec.setAttribute("to", `${secondsDegrees + 360} 579.15 400`);
-    console.log(sec)
+    sec.setAttribute("from", `${secondsDegrees} 579.15 378`);
+    sec.setAttribute("to", `${secondsDegrees + 360}  579.15 378`);
+    console.log(sec) 
 
 //     // 2 - minutes 
-//     const minutes = now.getMinutes();
-//     const minutesDegrees = ((minutes / 60) * 360) + 90;
-//     clockMinute.style.transform = `rotate(${minutesDegrees}deg)`;
-   
-//     // 3 - hours
-//     const hours = now.getHours();
-//     const hoursDegrees = ((hours / 12) * 360) + 90;
-//     clockHour.style.transform = `rotate(${hoursDegrees}deg)`; 
+    const minutes = now.getMinutes();
+    const minutesDegrees = ((minutes / 60) * 360) + 90;
+    const min = document.querySelector("#min-hand > animateTransform");
+    min.setAttribute("from", `${minutesDegrees} 579.15 378`);
+    min.setAttribute("to", `${minutesDegrees + 360} 579.15 378`);
+
+    
+    // 3 - hours
+    const hours = now.getHours();
+    const hoursDegrees = ((hours / 12) * 360) + 90;
+    const hour = document.querySelector("#min-hand > animateTransform");
+    hour.setAttribute("from", `${hoursDegrees} 579.15 378`);
+    hour.setAttribute("to", `${hoursDegrees + 360} 579.15 378`);
+
 
 
 // }
