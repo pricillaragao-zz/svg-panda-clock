1dfaecd33aaf2b11ccc76bc0f8d5169083309746
Pricilla Aragao <pricillapatricia@gmail.com>
2020-05-24 16:31:21 -0300

    center the clock on the page

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ min.setAttribute("to", `${minutesDegrees + 360} 579.15 378`);
 
 // 3 - hours
 const hours = now.getHours();
-const hoursDegrees = (hours * 3600 + minutes * 60 + seconds) * 360 / 43200;
+const hoursDegrees = ((hours * 3600 + minutes * 60 + seconds) * 360) / 43200;
 const hour = document.querySelector("#hour-hand > animateTransform");
 hour.setAttribute("from", `${hoursDegrees} 579.15 378`);
 hour.setAttribute("to", `${hoursDegrees + 360} 579.15 378`);
